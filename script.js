b0e791ada9271097a7c25ebc7d1042baffd64ceb
John Morgan <johnmorgan@uk.ibm.com>
2015-11-04 19:23:23 +0000

    save wrong and correct answers in to an array

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
 var score = 0
+var correctAnswer = [];
+var wrongAnswer = [];
 
 function answerFunction() {
   if (question === answer) {
     score++
     alert("CORRECT! your score is " + score);
+    correctAnswer.push(question);
   
   } else {
     alert("WRONG!!");
+    wrongAnswer.push(question);
   }
 };
 
 alert("Hello this is the ultimate quiz, there are 5 questions to follow....lets see how you do!");
 
 //QUESTION 1
-var question = prompt("What is the capital of France?");
+var question = "What is the capital of France?";
+prompt(question);
 var answer = "Paris";
 answerFunction();
 
 //QUESTION 2
-var question = prompt("How many days in a year?");
+var question = "How many days in a year?";
+prompt(question);
 var answer = "365";
 answerFunction();
 
 //QUESTION 3
-var question = prompt("What is 2+2?");
+var question = "What is 2+2?";
+prompt(question);
 var answer = "4";
 answerFunction();
 
 //QUESTION 4
-var question = prompt("How many days in a week?");
+var question = "How many days in a week?";
+prompt(question);
 var answer = "7";
 answerFunction();
 
 //QUESTION 5
-var question = prompt("What is the ocean between America and the UK?");
+var question = "What is the ocean between America and the UK?";
+prompt(question);
 var answer = "Atlantic Ocean";
 answerFunction();
 
@@ -53,4 +62,11 @@ if (score === 5) {
 } else {
   alert("The quiz is over you answered " + score + " questions correctly. This was not good enough to win any crowns");
 };
+
+function print(message) {
+  document.write(message);
+}
+
+print( "These are all the questions you got right" + correctAnswer.toString() );
+print( "These are all the questions you got wrong" + wrongAnswer.toString() );
 
